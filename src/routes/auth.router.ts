fecab051820433b5df4a311720b9cd56e0bb8830
hadinurr7 <hadinurr7@gmail.com>
2025-01-18 14:25:09 +0700

    feat: login

--- src/routes/auth.router.ts
+++ src/routes/auth.router.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { validateRegister } from "../validator/auth.validator";
-import { registerController } from "../controller/auth.controller";
+import { validateLogin, validateRegister } from "../validator/auth.validator";
+import { loginController, registerController } from "../controller/auth.controller";
 
 const router = Router();
 
 router.post("/register", validateRegister, registerController);
+router.post("/login", validateLogin, loginController)
 
 export default router;
